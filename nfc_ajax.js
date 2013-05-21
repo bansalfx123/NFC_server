4159b1b2d16286eae7a1d33156ad849a6f02e21c
fatkahawai <ceeb@burning-plains.com>
2013-05-21 12:43:49 +1200

    got http client, tcp client and server working

--- nfc_ajax.js
+++ nfc_ajax.js
@@ -3,11 +3,11 @@
  * a JSON API request - an ajax request to a HTTP server which returns a JSON object 
  * 
  * When a user browses to http://localhost:3000, index.html is loaded, which then 
- * loads and executes this code
+ * loads and executes this code to list  all the transactions, newest at the top
  *
  *  the JSON object return by the API is of the form 
- *  { 'tag1' : 'value',
- *    'tag2' : 'value',
+ *  { ID  : '01',
+ *    tag : 'message',
  *  }
  * @author Robert Drummond
  * Copyright (c) 2013 Pink Pelican NZ Ltd <bob@pink-pelican.com>
@@ -19,15 +19,22 @@ window.onload = function() {
         data,
         jqxhr;  
 
-    var txId = 'TEST 001';
+    var txId = 0;
 
     // Encode the user's input as query parameters in a URL
-    var url = document.URL+'tx/'+txId; 
+    var url = document.URL+'tx/'; 
+    // var url += txId;  // to select just one entry in txList table
+
+    console.log('sending Ajax query to '+url);    
 
     // send API request to server URL as HTTP GET
     jqxhr = $.getJSON( url, function(data) {
-        console.log('API response received');    
-        $('#transaction').html('transaction '+data['tag1']+' '+data['tag2']);        
+        
+        $('#transaction').html('<p></p>');
+        console.log('API response received'+JSON.stringify(data)+' length= '+data.length);    
+        for( var i=(data.length-1) ; i >= 0; i-- ) {
+          $('#transaction').append('<p>'+data[i].ID+' '+data[i].tag+'</p>');
+        }
       } 
     ); 
 }; 
--- nfc_server.js
+++ nfc_server.js
@@ -6,9 +6,13 @@
  * DESCRIPTION:
  * A node web Appserver running on port 3000
  * Uses the Express node package. 
- * serves the index.html and client-side Javascript files
- * responds to AJAX requests to the API with a JSON object containing an NFC transaction 
+ * serves the index.html and client-side Javascript files to a web browser.
+ * responds to Aclient-side JAX requests to the server API with a JSON object 
+ * containing an NFC transaction. 
  * 
+ * also opens a Socket and listens on port 51717 for TCP messages from the 
+ * Raspbery Pi.
+ *
  * @throws none
  * @see nodejs.org
  * @see express.org
@@ -17,14 +21,28 @@
  * Copyright (c) 2013 Pink Pelican NZ Ltd <bob@pink-pelican.com>
  */
 
-var http      = require('http');
-var express   = require('express');
+// Node modules
+//
+var http       = require('http');
+var express    = require('express');
+var net        = require('net');    // node sockets
+var JsonSocket = require('json-socket');
 
-var app       = express();
+// servers 
+//
+var app        = express();  // http server
+var tcpServer;              // tcp server
 
-var txList    = [ { tag1: 'Tom',   tag2: 'Bosley'},
-                  { tag1: 'James', tag2: 'Kirk'}
-                ];
+// port addresses
+//
+var tcpPortNo  = 51717;      // tcp socket port number
+var httpPortNo = 3000;       // http port number
+
+// NFC transactions list
+//
+var txList     = [ { ID: '01', tag: 'Tom Bosley'},
+                   { ID: '02', tag: 'James Kirk'}
+                 ];
 
 // ------------------------------------------------------------------------
 // configure Express to serve index.html and any other static pages stored 
@@ -42,14 +60,14 @@ var txList    = [ { tag1: 'Tom',   tag2: 'Bosley'},
 // Express route for incoming requests for a customer name
 app.get('/tx/:id', function(req, res){
   // send an object as a JSON string
- console.log('id = '+req.params.id);
+ console.log('API request for transaction id = '+req.params.id);
  res.send(txList[req.params.id]);
 }); // apt.get()
 
 // Express route for incoming requests for a list of all customers
 app.get('/tx', function(req, res){
   // send an object as a JSON string
-  console.log('all transactions');
+  console.log('API request for all transactions');
   res.send(txList);
 }); // apt.get()
 
@@ -61,15 +79,68 @@ app.get('*', function(req, res){
 // Express route to handle errors
 app.use(function(err, req, res, next){
   if (req.xhr) {
-    res.send(500, 'Oops, Something went wrong!');
+    res.send(500, 'Oops, Something went wrong with our API!');
   } else {
     next(err);
   }
 }); // apt.use()
 
+console.log('\n=======================================================================');
 // ------------------------------------------------------------------------
 // Start Express App Server on port 3000
 //
-app.listen(3000);
-console.log('App Server is listening on port 3000');
+app.listen( httpPortNo );
+console.log('HTTP App Server is listening on port '+httpPortNo);
+
+// ------------------------------------------------------------------------
+// Open Socket and listen on port 51717
+//
+tcpServer = net.createServer(function(socket) { 
+  console.log('TCP server connected on ' + socket.remoteAddress +':'+ socket.remotePort);
+
+  socket = new JsonSocket(socket); //Now we've decorated the net.Socket to be a JsonSocket
+
+  socket.on('message', function(message) {
+    console.log( 'TCP MESSAGE: received JSON: '+ message); 
+//    socket.sendMessage({msg: 'ACK'});
+  });
+
+  socket.on('data', function(data) {
+    console.log('TCP DATA: TCP server received JSON string '+data);
+
+    // process JSON data here
+    var tx = JSON.parse(data);
+    console.log('parsed JSON result: data.ID= '+tx.ID+' data.tag= '+tx.tag);
+
+    txList.push(tx);
+    //console.log('full txList: '+JSON.stringify(txList));
+
+    socket.sendMessage('ACK'); 
+    console.log('TCP send: sent ACK to client');
+  });
+
+  socket.on('end', function() {
+    console.log('TCP END: TCP client disconnected');
+  });
+
+});
+
+tcpServer.listen(tcpPortNo, function() { //'listening' listener
+  console.log('server listening on port '+tcpPortNo);
+  var address = tcpServer.address();
+  console.log("opened server on %j", address);
+
+});
+
+tcpServer.on('error', function (e) {
+    if (e.code == 'EADDRINUSE') {
+      console.log('ERROR: Port Address in use, retrying...');
+      setTimeout(function () {
+        tcpServer.close();
+        tcpServer.listen(tcpPortNo);
+      }, 1000);
+    }
+});
+
+// tcpServer.close();
 
